b478e71ae8ebcf8f884e5f73054bae40cf41b2db
Ben Zimmer <benjamin.zimmer@infopark.de>
2012-07-14 16:36:37 +0200

    first working state

--- public/javascripts/sciime.js
+++ public/javascripts/sciime.js
@@ -1,18 +1,9 @@
 (function($) {
 
   Sciime = {
-    widget_list: [
-
-    ],
-
     configuration: {
-      timeout: 4000,
-      list_url: '/widget-list',
-      replacements: {
-        content: '#widget-content',
-        title: '#widget-title',
-        subtitle: '#widget-subtitle'
-      }
+      slide_interval: 2000,
+      list_url: '/widget-list'
     },
 
     widget_configuration : {
@@ -20,20 +11,47 @@
     },
 
     init: function() {
-      this.load_widget_list();
-      this.loop_widgets();
+      var self = this;
+      $('body').bind('widgets_loaded', function(event, widget_list) {
+        widget_list.forEach(function(widget_data) {
+          self.load_widget(widget_data.filename).done(function(widget_html) {
+            var item = $('<div>').addClass('item');
+            var widget_header = $('<div>').addClass('hero-unit');
+            item.append(widget_html);
+
+            var carousel = $('.carousel-inner');
+            carousel.append(item);
+
+            var widget_configuration = eval(widget_data.namespace).configuration;
+            widget_configuration.filename = widget_data.filename;
+            widget_configuration.namespace = widget_data.namespace;
+
+            var widget_title = $('<h1>').text(widget_configuration.title);
+            var widget_subtitle = $('<p>').text(widget_configuration.subtitle);
+
+            widget_header.append(widget_title);
+            widget_header.append(widget_subtitle);
+            item.prepend(widget_header);
+            item.data('widget', widget_configuration);
+
+            if (carousel.find('.item').length === widget_list.length) {
+              self.init_widgets();
+            }
+          });
+        });
+      });
+
+      self.load_widget_list();
     },
 
-    loop_widgets: function(widget_list) {
+    init_widgets: function() {
       var self = this;
-      var widget_count = 0;
-
-      setInterval(function() {
-        self.load_widget(self.widget_list[widget_count]);
-
-        widget_count++;
-        widget_count = widget_count % self.widget_list.length
-      }, self.configuration.timeout)
+      var carousel = $('.carousel');
+      carousel.carousel({interval: self.configuration.slide_interval});
+      carousel.bind('slid', function(event) {
+        var widget_data = $(event.target).find('.active').data('widget');
+      });
+      $('body').trigger('widgets_initialized');
     },
 
     load_widget_list: function() {
@@ -41,23 +59,17 @@
 
       $.ajax({
         url: self.configuration.list_url,
-        async: false
+        dataType: 'json'
       }).done(function(data) {
-        self.widget_list = jQuery.parseJSON(data);
+        $('body').trigger('widgets_loaded', [data]);
       });
     },
 
-    load_widget: function(slide) {
+    load_widget: function(filename) {
       var self = this;
 
-      $.ajax({
-        url: '/widgets/' + slide
-      }).done(function(data) {
-        self.widget_configuration.content = data;
-
-        $.each(self.configuration.replacements, function(key, value) {
-          $(value).html(self.widget_configuration[key]);
-        });
+      return $.ajax({
+        url: '/widgets/' + filename + '.html'
       });
     },
 
